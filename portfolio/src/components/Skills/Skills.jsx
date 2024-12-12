110a3a2070f72e18fb5dff84484c7ff8ea0f493f
Lavicka13 <luky.lavicka@gmail.com>
2024-12-12 11:03:07 +0100

    clickable skillicons

--- portfolio/src/components/Skills/Skills.jsx
+++ portfolio/src/components/Skills/Skills.jsx
@@ -2,51 +2,49 @@ import React from 'react';
 import { Grid, Title, Tooltip, Image, Container } from '@mantine/core';
 import './skills.css';
 
-
 const skills = [
-  { title: 'C#', picture: '/assets/icons/c.svg' },
-  { title: 'React', picture: '/assets/icons/react.svg' },
-  { title: 'GIT', picture: '/assets/icons/git.png' },
-  { title: 'JavaScript', picture: '/assets/icons/js.png' },
-  { title: 'HTML', picture: '/assets/icons/html.svg' },
-  { title: 'CSS', picture: '/assets/icons/css.svg' },
-  { title: 'Visual Studio Code', picture: '/assets/icons/visual-studio-code.svg' },
-  { title: 'MySQL', picture: '/assets/icons/MySQL_logo.svg' },
-  { title: 'Mantine', picture: '/assets/icons/mantine.com.png' },
-  { title: 'Figma', picture: '/assets/icons/figma.png' },
-  { title: 'Canva', picture: '/assets/icons/canva.svg' },
-  { title: 'Slack', picture: '/assets/icons/slack.svg' },
+  { title: 'C#', picture: '/assets/icons/c.svg', url: 'https://learn.microsoft.com/en-us/dotnet/csharp/' },
+  { title: 'React', picture: '/assets/icons/react.svg', url: 'https://reactjs.org/' },
+  { title: 'GIT', picture: '/assets/icons/git.png', url: 'https://git-scm.com/' },
+  { title: 'JavaScript', picture: '/assets/icons/js.png', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
+  { title: 'HTML', picture: '/assets/icons/html.svg', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
+  { title: 'CSS', picture: '/assets/icons/css.svg', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
+  { title: 'Visual Studio Code', picture: '/assets/icons/visual-studio-code.svg', url: 'https://code.visualstudio.com/' },
+  { title: 'MySQL', picture: '/assets/icons/MySQL_logo.svg', url: 'https://www.mysql.com/' },
+  { title: 'Mantine', picture: '/assets/icons/mantine.com.png', url: 'https://mantine.dev/' },
+  { title: 'Figma', picture: '/assets/icons/figma.png', url: 'https://www.figma.com/' },
+  { title: 'Canva', picture: '/assets/icons/canva.svg', url: 'https://www.canva.com/' },
+  { title: 'Slack', picture: '/assets/icons/slack.svg', url: 'https://slack.com/' },
 ];
 
 function Skills() {
   return (
     <section id="skills">
-    <Container>
-    <div style={{ height: '20px', marginTop: '-50px' }} aria-hidden="true"></div>
-      <Title className = 'title'>Skills</Title>
-      <Grid gutter="xl">
-        {skills.map((skill, index) => {
-          //const isJavaScript = skill.title === 'JavaScript'; -> wird nicht mehr benötigt
-          
-          return (
-            <Grid.Col key={index} span={4}>
-              <Tooltip label={skill.title} position="top" withArrow>
-                <div className="icon-wrapper" >
-                  <Image
-                    src={skill.picture}
-                    alt={skill.title}
-                    width={80}
-                    height={80}
-                    className="icon"
-                    //style={isJavaScript ? { width:'150px' , height:'130px'} : {}} -> wird nicht benötigt
-                  />
-                </div>
-              </Tooltip>
-            </Grid.Col>
-          );
-        })}
-      </Grid>
-    </Container>
+      <Container>
+        <div style={{ height: '20px', marginTop: '-50px' }} aria-hidden="true"></div>
+        <Title className="title">Skills</Title>
+        <Grid gutter="xl">
+          {skills.map((skill, index) => {
+            return (
+              <Grid.Col key={index} span={4}>
+                <Tooltip label={skill.title} position="top" withArrow>
+                  <a href={skill.url} target="_blank" rel="noopener noreferrer" className="icon-link">
+                    <div className="icon-wrapper">
+                      <Image
+                        src={skill.picture}
+                        alt={skill.title}
+                        width={80}
+                        height={80}
+                        className="icon"
+                      />
+                    </div>
+                  </a>
+                </Tooltip>
+              </Grid.Col>
+            );
+          })}
+        </Grid>
+      </Container>
     </section>
   );
 }
--- portfolio/src/components/SocialBar/socialbar.jsx
+++ portfolio/src/components/SocialBar/socialbar.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Group } from '@mantine/core';
-import { IconBrandGithub, IconBrandLinkedin, IconBrandTwitter } from '@tabler/icons-react';
+import { IconBrandGithub, IconMail, IconDeviceMobile } from '@tabler/icons-react';
+
 import './socialbar.css'; // Für die Stildefinition
 
 function SocialBar() {
   return (
     <div className="social-bar">
       <Group direction="column" spacing="sm" position="center">
+        {/* GitHub Link */}
         <a href="https://github.com/Lavicka13" target="_blank" rel="noopener noreferrer">
           <IconBrandGithub size={32} color="white" />
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-          <IconBrandLinkedin size={32} color="white" />
+        
+        {/* E-Mail Link */}
+        <a href="mailto:lukaslavick@gmail.com" target="_blank" rel="noopener noreferrer">
+          <IconMail size={32} color="white" />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <IconBrandTwitter size={32} color="white" />
+        
+        {/* Telefon Link */}
+        <a href="tel:+4915256801795" target="_blank" rel="noopener noreferrer">
+          <IconDeviceMobile size={32} color="white"/>
         </a>
       </Group>
     </div>
